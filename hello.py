8b7fbe4b38cc348563fc4f90dacb2b18f60947bc
UnrealJames <unrealjames@outlook.com>
2026-05-28 20:15:17 -0400

    add hello.py

---
+++ hello.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+goal = input("What is your goal? ")
+
+print(f"Hello, my name is {name} and I am working towards becoming an {goal}")
